be90f3e5a5e3494998489559a99053e0a0e4ec05
Chaitanya Uttarwar <chitti-chitti@users.noreply.github.com>
2018-08-20 17:10:33 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'what is my bill':
+			$speech = "According to records its $650";
+			break;
+
+		case 'When is my due date':
+			$speech = "Your due date is 13th November 2018 after which you will be charged $50 delay fee.";
+			break;
+			
+		case 'When is the nect billing cycle':
+			$speech = "November is the next cycle";
+			break;
+			
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
