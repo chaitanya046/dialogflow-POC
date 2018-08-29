198af15e6da4ac588dfa4a411cee5e53bfdc8143
Chaitanya Uttarwar <chitti-chitti@users.noreply.github.com>
2018-08-29 15:37:32 +0530

    update

--- index.php
+++ index.php
@@ -12,15 +12,15 @@ if($method == 'POST'){
 			$speech = "Hi, Nice to meet you";
 			break;
 
-		//case 'what is my bill'||'what is my bill amount'||'whats my bill'||'bill amount'||'my bill'||'what\'s my bill'||'whats my bill':
-			//$speech = "According to records its $650";
-			//break;
+		case "what is my bill":
+			$speech = "According to records its $650";
+			break;
 
-		case 'when is its due date'||'when is my due date'||'when is the due date'||'when is due date':
+		case "when is its due date":
 			$speech = "Your due date is 13th November 2018 after which you will be charged $50 delay fee.";
 			break;
 			
-		case 'when is the next billing cycle'||'when is next billing cycle'||'next cycle for billing'||'next billing':
+		case "when is the next billing cycle":
 			$speech = "November is the next cycle";
 			break;
 			
